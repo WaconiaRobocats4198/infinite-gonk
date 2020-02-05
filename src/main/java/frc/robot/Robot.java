903631c8404dcc0d6d772e6ff63bbcd1d4735924
WaconiaRobocats4198 <47279266+WaconiaRobocats4198@users.noreply.github.com>
2020-02-04 19:08:24 -0600

    auto WIP

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -10,7 +10,9 @@ import edu.wpi.first.wpilibj.Solenoid;
 
 import com.ctre.phoenix.motorcontrol.can.*;
 
-import com.revrobotics.CANSparkMaxLowLevel;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANEncoder;
 
 
 public class Robot extends TimedRobot {
@@ -21,12 +23,25 @@ public class Robot extends TimedRobot {
   public static WPI_TalonSRX frontRight = new WPI_TalonSRX(5);
   public static WPI_TalonSRX backLeft = new WPI_TalonSRX(6);
   public static WPI_TalonSRX backRight = new WPI_TalonSRX(7);
+
+  public static CANSparkMax frontL = new CANSparkMax(1, MotorType.kBrushless);
+  public static CANSparkMax frontR = new CANSparkMax(2, MotorType.kBrushless);
+  public static CANSparkMax backL = new CANSparkMax(3, MotorType.kBrushless);
+  public static CANSparkMax backR = new CANSparkMax(4, MotorType.kBrushless);
+
+  public static CANEncoder flEnc = new CANEncoder(frontL);
+  public static CANEncoder frEnc = new CANEncoder(frontR);
+  public static CANEncoder blEnc = new CANEncoder(backL);
+  public static CANEncoder brEnc = new CANEncoder(backR);
   
   public static MecanumDrive scoot = new MecanumDrive(frontLeft, backLeft, frontRight, backRight);
 
   limelight vision = new limelight();
 
   colorParse colorWheel = new colorParse();
+  public static void launch(){
+    
+  }
  
   @Override
   public void robotInit() {
--- src/main/java/frc/robot/autoBlocks.java
+++ src/main/java/frc/robot/autoBlocks.java
@@ -1,9 +1,9 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.drive.MecanumDrive;
-import com.revrobotics.CANSparkMaxLowLevel;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANEncoder;
-
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
@@ -14,35 +14,70 @@ public class autoBlocks {
     limelight autoLime = new limelight();
 
     launchAngler launcheyBoi = new launchAngler();
-    public void forward(double inches){
-        //double startPos = Robot.flEnc.getPosition();
-        double rotations =inches/(8 * Math.PI);
+    public void straight(double inches){
+        double startPos = Robot.flEnc.getPosition();
+        double encTicks = distaneToEnc(inches);
 
-        double encTicks = rotations * 42;
-
-        double time = System.currentTimeMillis();
-        // if(Robot.flEnc.getPosition() <  startPos + encTicks){
-        //     if(System.currentTimeMillis() < time + 500){
-        //         Robot.scoot.driveCartesian(0, ((System.currentTimeMillis()-time) / 500) * 0.7 + 0.3, 0);
-        //     }
-        //     else if(Robot.flEnc.getPosition() <  (startPos + encTicks) - 200){
-        //         Robot.scoot.driveCartesian(0, 1-(((startPos + encTicks)-Robot.flEnc.getPosition())/200) * 0.7, 0);
-        //     }
-        //     else{
-        //         Robot.scoot.driveCartesian(0, 1, 0);
-        //     }
-        // }
-        // else{
-        //     Robot.scoot.driveCartesian(0, 0, 0);
-        // }
+        if(Robot.flEnc.getPosition() <  startPos + encTicks){
+            Robot.scoot.driveCartesian(0, 1, 0);
+        }
+        else{
+            Robot.scoot.driveCartesian(0, 0, 0);
+        }
     }
-    public void arc(double degrees, double radius){
+    public void arc(double degrees, double radius, String direction){
+        double outerDistance = (radius + 9.75) * 2 * Math.PI;
+        double innerDistance = (radius - 9.75) * 2 * Math.PI;
+
+        double speedRatio = innerDistance/outerDistance;
+        double leftEnc = 0;
+        double rightEnc = 0;
+        boolean sideSpeed = true;
+
+        if (direction == "right"){
+            leftEnc = Robot.flEnc.getPosition() + distaneToEnc(outerDistance) * (degrees/360);
+            rightEnc = Robot.frEnc.getPosition() + distaneToEnc(innerDistance) * (degrees/360);
+            sideSpeed = true;
+        }
+        else if(direction == "left"){
+            rightEnc = Robot.frEnc.getPosition() + (outerDistance) * (degrees/360);
+            leftEnc = Robot.flEnc.getPosition() + (innerDistance) * (degrees/360);
+            sideSpeed = false;
+        }
+
+        if(Robot.flEnc.getPosition() < leftEnc){
+            if(sideSpeed == true){
+                Robot.frontL.set(1);
+                Robot.backL.set(1);
+            }
+            else{
+                Robot.frontL.set(speedRatio);
+                Robot.backL.set(speedRatio);
+            }
+        }
+        if(Robot.frEnc.getPosition() < rightEnc){
+            if(sideSpeed == false){
+                Robot.frontR.set(1);
+                Robot.backR.set(1);
+            }
+            else{
+                Robot.frontR.set(speedRatio);
+                Robot.backR.set(speedRatio);
+            }
+        }
 
     }
     public void fullAuto(int position){
         if(position == 1){
             autoCam();
             launcheyBoi.tip();
+            Robot.launch();
+            straight(-70);
+            autoCam();
+            straight(-200);
+        }
+        if(position == 2){
+            
         }
     }
     public void autoCam(){
@@ -52,6 +87,13 @@ public class autoBlocks {
         else if(autoLime.xTranslate < 0){
             
         }
+    }
+    public double distaneToEnc(double distance){
+        double encCount;
+        double rotations =distance/(8 * Math.PI);
+        encCount = rotations * 42;
+       
+        return encCount;
     }
 
 }

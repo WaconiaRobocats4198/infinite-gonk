05e5389016ece9d492c9b7e8359fc295899d960f
WaconiaRobocats4198 <47279266+WaconiaRobocats4198@users.noreply.github.com>
2020-02-15 15:28:37 -0600

    stress

--- src/main/java/frc/robot/autoBlocks.java
+++ src/main/java/frc/robot/autoBlocks.java
@@ -1,108 +1,110 @@
-// package frc.robot;
+package frc.robot;
 
-// // import edu.wpi.first.wpilibj.drive.MecanumDrive;
-// // import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-// // import com.revrobotics.CANSparkMax;
-// // import com.revrobotics.CANEncoder;
-// // import edu.wpi.first.networktables.NetworkTable;
-// // import edu.wpi.first.networktables.NetworkTableEntry;
-// // import edu.wpi.first.networktables.NetworkTableInstance;
-// // import edu.wpi.first.wpilibj.shuffleboard.*;
+// import edu.wpi.first.wpilibj.drive.MecanumDrive;
+// import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+// import com.revrobotics.CANSparkMax;
+// import com.revrobotics.CANEncoder;
+// import edu.wpi.first.networktables.NetworkTable;
+// import edu.wpi.first.networktables.NetworkTableEntry;
+// import edu.wpi.first.networktables.NetworkTableInstance;
+// import edu.wpi.first.wpilibj.shuffleboard.*;
 
 
-// public class autoBlocks {
-//     limelight autoLime = new limelight();
+public class autoBlocks {
+    limelight autoLime = new limelight();
 
-//     launchAngler launcheyBoi = new launchAngler();
-//     public void straight(double inches){
-//         double startPos = Robot.flEnc.getPosition();
-//         double encTicks = distaneToEnc(inches);
+    launchAngler launcheyBoi = new launchAngler();
+    public void straight(double inches){
+        double startPos = Robot.flEnc.getPosition();
+        double encTicks = distaneToEnc(inches);
 
-//         if(Robot.flEnc.getPosition() <  startPos + encTicks){
-//             Robot.scoot.driveCartesian(0, 1, 0);
-//         }
-//         else{
-//             Robot.scoot.driveCartesian(0, 0, 0);
-//         }
-//     }
-//     public void arc(double degrees, double radius, String direction){
-//         double outerDistance = (radius + 9.75) * 2 * Math.PI;
-//         double innerDistance = (radius - 9.75) * 2 * Math.PI;
+        if(Robot.flEnc.getPosition() <  startPos + encTicks){
+            Robot.scoot.driveCartesian(0, 1, 0);
+        }
+        else{
+            Robot.scoot.driveCartesian(0, 0, 0);
+        }
+    }
+    public void arc(double degrees, double radius, String direction){
+        double outerDistance = (radius + 9.75) * 2 * Math.PI;
+        double innerDistance = (radius - 9.75) * 2 * Math.PI;
 
-//         double speedRatio = innerDistance/outerDistance;
-//         double leftEnc = 0;
-//         double rightEnc = 0;
-//         boolean sideSpeed = true;
+        double speedRatio = innerDistance/outerDistance;
+        double leftEnc = 0;
+        double rightEnc = 0;
+        boolean sideSpeed = true;
 
-//         if (direction == "right"){
-//             leftEnc = Robot.flEnc.getPosition() + distaneToEnc(outerDistance) * (degrees/360);
-//             rightEnc = Robot.frEnc.getPosition() + distaneToEnc(innerDistance) * (degrees/360);
-//             sideSpeed = true;
-//         }
-//         else if(direction == "left"){
-//             rightEnc = Robot.frEnc.getPosition() + (outerDistance) * (degrees/360);
-//             leftEnc = Robot.flEnc.getPosition() + (innerDistance) * (degrees/360);
-//             sideSpeed = false;
-//         }
+        if (direction == "right"){
+            leftEnc = Robot.flEnc.getPosition() + distaneToEnc(outerDistance) * (degrees/360);
+            rightEnc = Robot.frEnc.getPosition() + distaneToEnc(innerDistance) * (degrees/360);
+            sideSpeed = true;
+        }
+        else if(direction == "left"){
+            rightEnc = Robot.frEnc.getPosition() + (outerDistance) * (degrees/360);
+            leftEnc = Robot.flEnc.getPosition() + (innerDistance) * (degrees/360);
+            sideSpeed = false;
+        }
 
-//         if(Robot.flEnc.getPosition() < leftEnc){
-//             if(sideSpeed == true){
-//                 Robot.frontL.set(1);
-//                 Robot.backL.set(1);
-//             }
-//             else{
-//                 Robot.frontL.set(speedRatio);
-//                 Robot.backL.set(speedRatio);
-//             }
-//         }
-//         if(Robot.frEnc.getPosition() < rightEnc){
-//             if(sideSpeed == false){
-//                 Robot.frontR.set(1);
-//                 Robot.backR.set(1);
-//             }
-//             else{
-//                 Robot.frontR.set(speedRatio);
-//                 Robot.backR.set(speedRatio);
-//             }
-//         }
+        if(Robot.flEnc.getPosition() < leftEnc){
+            if(sideSpeed == true){
+                Robot.frontL.set(1);
+                Robot.backL.set(1);
+            }
+            else{
+                Robot.frontL.set(speedRatio);
+                Robot.backL.set(speedRatio);
+            }
+        }
+        if(Robot.frEnc.getPosition() < rightEnc){
+            if(sideSpeed == false){
+                Robot.frontR.set(1);
+                Robot.backR.set(1);
+            }
+            else{
+                Robot.frontR.set(speedRatio);
+                Robot.backR.set(speedRatio);
+            }
+        }
 
-//     }
-//     public void fullAuto(int position){
-//         if(position == 1){
-//             autoCam();
-//             launcheyBoi.tip();
-//             Robot.launch();
-//             straight(-70);
-//             autoCam();
-//             launcheyBoi.tip();
-//             straight(-200);
-//         }
-//         if(position == 2){
-//             straight(-35);
-//             if(autoLime.xTranslate > 1 || autoLime.xTranslate < -1){
-//                 Robot.scoot.driveCartesian(0, 1, 0);
-//             }
-//             else{
-//                 Robot.scoot.driveCartesian(0, 0, 0);
-//             }
-//             launcheyBoi.innerSet();
-//             straight(-200);
-//         }
-//     }
-//     public void autoCam(){
-//         if(autoLime.xTranslate > 0){
+    }
+    public void fullAuto(int position){
+        if(position == 1){
+            autoCam();
+            do{
+                launcheyBoi.tip();
+                launcheyBoi.autoLaunchTime();
+                Robot.launch(2);
+            }while(Robot.ballCount <3);
+            straight(-70);
+            autoCam();
+            launcheyBoi.tip();
+            straight(-200);
+        }
+        if(position == 2){
+            straight(-35);
+            if(autoLime.xTranslate > 1 || autoLime.xTranslate < -1){
+                Robot.scoot.driveCartesian(0, 1, 0);
+            }
+            else{
+                Robot.scoot.driveCartesian(0, 0, 0);
+            }
+            launcheyBoi.innerSet();
+            straight(-200);
+        }
+    }
+    public void autoCam(){
+        if(autoLime.xTranslate > 0){
             
-//         }
-//         else if(autoLime.xTranslate < 0){
+        }
+        else if(autoLime.xTranslate < 0){
             
-//         }
-//     }
-//     public double distaneToEnc(double distance){
-//         double encCount;
-//         double rotations =distance/(8 * Math.PI);
-//         encCount = rotations * 42;
+        }
+    }
+    public double distaneToEnc(double distance){
+        double encCount;
+        double rotations =distance/(8 * Math.PI);
        
-//         return encCount;
-//     }
+        return rotations;
+    }
 
-// }
+}
--- src/main/java/frc/robot/launchAngler.java
+++ src/main/java/frc/robot/launchAngler.java
@@ -1,14 +1,39 @@
 package frc.robot;
+import com.revrobotics.CANPIDController;
+import com.revrobotics.ControlType;
 
 public class launchAngler {
     limelight ranger = new limelight();
     double range = ranger.rangeFinder();
+    double startAngle;
+    boolean launchLast = false;
+    public double positionTarget;
     public void tip(){
         
     }
-    public void innerSet(){
+    public void angleSet(double targetAngle){
+        Robot.pitcherPID.setP(1e-2);
+        Robot.pitcherPID.setI(1e-6);
 
+        positionTarget = (targetAngle - startAngle)/5.07;
+
+        Robot.pitcherPID.setReference(positionTarget, ControlType.kPosition);
     }
+    public void innerSet(){
+        double innerHoleAngle = 21;
+        angleSet(21);
+    }
+    public void autoLaunchTime(){
+        if(Math.abs(Robot.pitchEnc.getPosition() - positionTarget) < 0.2){
+            Robot.launchStatus = true;
+        }
+        if(Robot.launchStatus == true && launchLast == false){
+            Robot.launchCountdown = System.currentTimeMillis() + Robot.launchWait;
+        }
+        launchLast = Robot.launchStatus;
+        
+    }
+
 
 
 }
